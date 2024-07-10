fd4a86ac3686a22427e38ac0b1eddb77b733c5e0
akmaalh <akmalhakim2106@gmail.com>
2024-07-10 18:00:53 +0700

    Initial Commit

---
+++ tokolator/tokolatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tokolatorApp.swift
+//  tokolator
+//
+//  Created by Akmal Hakim on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct tokolatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
