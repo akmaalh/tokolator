4882f68967266fa08b33bcb15f7ba54489dbba3c
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2024-07-11 14:17:51 +0700

    feat: add inventory page

---
+++ tokolator/View/InventoryView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+import SwiftData
+
+struct InventoryView: View {
+    @Query(sort: \Item.timestamp, order: .reverse) private var items: [Item]
+    @State private var selectedItem: Item? = nil
+    @State private var isRestockViewPresented: Bool = false
+    @State private var isAddItemViewPresented: Bool = false
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(items) { item in
+                    Button(action: {
+                        selectedItem = item
+                    }) {
+                        Text("\(item.name) [\(item.stock)]")
+                            .foregroundColor(Color.primary)
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    isAddItemViewPresented = true
+                }, label: {
+                    Text("Add Item")
+                })
+                .buttonStyle(.borderedProminent)
+                
+                Button(action: {
+                    isRestockViewPresented = true
+                }, label: {
+                    Text("Restock")
+                })
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
+        }
+        .sheet(item: $selectedItem) { item in
+//            ItemDetailView(item: item)
+        }
+        .sheet(isPresented: $isRestockViewPresented) {
+//            RestockView()
+        }
+        .sheet(isPresented: $isAddItemViewPresented) {
+//            AddItemView()
+        }
+    }
+}
+
+#Preview {
+    InventoryView()
+}
